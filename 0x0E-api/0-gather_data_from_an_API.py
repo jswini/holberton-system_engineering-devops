6a8b2e66456c033cd6464a387bb39eb6940232bf
jswini <jswinikoff@gmail.com>
2022-03-24 16:35:25 -0500

    moved all code so to be not executable if imported

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -2,32 +2,34 @@
 '''
 This script will gather and process data from a REST API
  and process it for display
- '''
+'''
 import requests
 import sys
 
-task_completed = []
-task_completed_count = 0
-employee_id = int(sys.argv[1])
-employees = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                         .format(employee_id))
-employees_info = employees.json()['name']
-# (alt way for above)employees_info = employees.get('name')
-all_tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                         .format(employee_id))
-task_list = all_tasks.json()
-
-task_count = len(task_list)
-for task in task_list:
-    if task.get('completed') is True:
-        task_completed.append(task.get('title'))
-    task_completed_count = len(task_completed)
-
 if __name__ == "__main__":
     '''
-    after data is found it is output
+    finds data from api
+    formats data
+    outputs data
     '''
-    print('Employee {} is done with tasks({}/{})'
-          .format(employees_info, task_completed_count, task_count))
-    for item in task_completed:
-        print('\t {}'.format(item))
+    task_completed = []
+    task_completed_count = 0
+    employee_id = int(sys.argv[1])
+    employees = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(employee_id))
+    employees_info = employees.json()['name']
+    # (alt way for above)employees_info = employees.get('name')
+    all_tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                            .format(employee_id))
+    task_list = all_tasks.json()
+
+    task_count = len(task_list)
+    for task in task_list:
+        if task.get('completed') is True:
+            task_completed.append(task.get('title'))
+        task_completed_count = len(task_completed)
+
+        print('Employee {} is done with tasks({}/{})'
+            .format(employees_info, task_completed_count, task_count))
+        for item in task_completed:
+            print('\t {}'.format(item))
